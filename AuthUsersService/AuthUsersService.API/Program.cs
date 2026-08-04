ba0a4edc205d66bc40c497fbbdef32941ccd8da1
Home <Home@Home>
2026-08-04 23:20:36 +0300

    [issue-17] Фикс билда

--- AuthUsersService/AuthUsersService.API/Program.cs
+++ AuthUsersService/AuthUsersService.API/Program.cs
@@ -3,7 +3,6 @@ using AuthUsersService.Application.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using AuthUsersService.Application.Services.External.UsersGrpc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,7 +30,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGrpcService<UsersGrpcService>();
+app.MapInternalGrpcServices();
+
 app.MapControllers();
 
 app.Run();
